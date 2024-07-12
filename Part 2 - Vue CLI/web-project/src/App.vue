f592e5bddf1ecf8cf5666930df7b9e6ea670bf6c
Kyle <kyledwalker95@gmail.com>
2024-07-12 15:54:26 -0400

    Added Buttons to open and close Modals

--- Part 2 - Vue CLI/web-project/src/App.vue
+++ Part 2 - Vue CLI/web-project/src/App.vue
@@ -1,23 +1,52 @@
 <template>
-  <h1>{{ title }}</h1>
-  <input
-    type="text"
-    ref="name" />
-  <button @click="handleClick">Click me!</button>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+
+  <div v-if="showModal">
+    <Modal
+      theme=""
+      @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Modal 1</h1>
+      <p>Modal 1 Text</p>
+    </Modal>
+  </div>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Modal 2</h1>
+      <p>Modal 2 Text</p>
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">Open Modal 1</button>
+  <button @click="toggleModalTwo">Open Modal 2</button>
 </template>
 
 <script>
-  import Modal from './components/Modal.vue'
+  import Modal from './components/Modal'
 
   export default {
     name: 'App',
+    components: { Modal },
     data() {
       return {
-        title: 'Maritzas Transportation',
+        title: 'Website Title',
+        showModal: false,
+        showModalTwo: false,
       }
     },
-    components: {
-      Modal,
+    methods: {
+      toggleModal() {
+        this.showModal = !this.showModal
+      },
+      toggleModalTwo() {
+        this.showModalTwo = !this.showModalTwo
+      },
     },
   }
 </script>
@@ -31,10 +60,18 @@
     color: #2c3e50;
     margin-top: 60px;
   }
-
   h1 {
     border-bottom: 1px solid #ddd;
     display: inline-block;
     padding-bottom: 10px;
+  }
+  button {
+    background: #bbb;
+    padding: 10px;
+    border: none;
+    border-radius: 4px;
+    margin: 10px;
+    font-size: 16px;
+    color: #333;
   }
 </style>
