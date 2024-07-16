13103f33cf818cb14b5f92400e8b235e5ba85f0d
Kyle <kyledwalker95@gmail.com>
2024-07-16 16:16:29 -0400

    Added Part 4 - Vue Forms

---
+++ Part 4 - Forms/web-form/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Useability Form</h1>
+  <SignupForm />
+</template>
+
+<script>
+  import SignupForm from './components/SignupForm.vue'
+  export default {
+    name: 'App',
+    components: { SignupForm },
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  body {
+    margin: 0px;
+    padding: 0px;
+    background: #eee;
+  }
+</style>
---
+++ Part 4 - Forms/web-form/src/components/SignupForm.vue
@@ -0,0 +1,141 @@
+<template>
+  <form>
+    <label for="">Email:</label>
+    <input
+      type="email"
+      v-model="email"
+      required />
+    <label for="">Password:</label>
+    <input
+      type="password"
+      v-model="password"
+      required />
+
+    <label for="">Role</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <label>Skills</label>
+    <input
+      type="text"
+      v-model="tempSkill"
+      @keyup="addSkill" />
+    <div
+      v-for="skill in skills"
+      :key="skill"
+      class="pill"
+      @click.self="handleDelete(skill)">
+      {{ skill }}
+    </div>
+
+    <div class="terms">
+      <input
+        type="checkbox"
+        required
+        v-model="terms" />
+      <label for="">I understand the Terms and Conditions</label>
+    </div>
+    <div class="newsletter">
+      <input
+        type="checkbox"
+        v-model="newsletter" />
+      <label for="">Sign up for our weekly newsletter</label>
+    </div>
+  </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Skills: {{ skills }}</p>
+  <div>
+    <p v-if="terms">The terms have been accepted!</p>
+    <p v-else>The terms have not been accepted!</p>
+  </div>
+  <p>Newsletter: {{ newsletter }}</p>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+        role: '',
+        terms: false,
+        newsletter: true,
+        tempSkill: [],
+        skills: [],
+        index: '',
+      }
+    },
+    methods: {
+      addSkill(e) {
+        if (e.key === 'Enter') {
+          if (!this.skills.includes(this.tempSkill)) {
+            this.skills.push(this.tempSkill)
+          }
+          this.tempSkill = ''
+        }
+      },
+
+      handleDelete(skill) {
+        this.index = this.skills.indexOf(skill)
+        this.skills.splice(this.index, 1)
+      },
+    },
+  }
+</script>
+
+<style>
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+
+  input,
+  select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+
+  input[type='checkbox'] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+  }
+</style>
